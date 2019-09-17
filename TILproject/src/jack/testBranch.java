9fbfa7ef0fb8fbde841ee652c4fbeb1f0c9909b4
jack0608 <dnfkrl11@gmail.com>
2019-09-17 18:01:58 +0900

    9. 머지가 안됐는데..?

--- TILproject/src/jack/testBranch.java
+++ TILproject/src/jack/testBranch.java
@@ -3,7 +3,7 @@ package jack;
 public class testBranch {
 	private void branchTest() {
 		// TODO Auto-generated method stub
-		System.out.println("branchTest");
+		System.out.println("branchTest 1");
 	}
 
 }
