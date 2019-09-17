a0d294aa6edfee240effc4c886f48dd7a6ffe6ad
jack0608 <dnfkrl11@gmail.com>
2019-09-17 17:25:25 +0900

    5. make package

---
+++ TILproject/src/jack/testCode.java
@@ -0,0 +1,8 @@
+package jack;
+
+public class testCode {
+	public void jack() {
+		// TODO Auto-generated method stub
+		System.out.println("jack test");
+	}
+}
---
+++ TILproject/src/jack/testController.java
@@ -0,0 +1,18 @@
+package jack;
+
+import jack.testCode;
+
+public class testController {
+	
+	public static void main(String[] args) {
+		System.out.println("test code commit");
+		
+		System.out.println("clone project");
+		
+		System.out.println("myproject");
+		
+		testCode test = new testCode();
+		
+		test.jack();
+	}
+}
